a0c48e918b44d0fba34159e57fd7763805f72bb7
sgmonusg <sagarguptamonu1996@gmail.com>
2016-12-17 20:46:11 +0530

    added Solution to ques2 to DSA LABSHEET 1

--- Lab Problems/java/week_1/ques_2.java
+++ Lab Problems/java/week_1/ques_2.java
@@ -1,5 +1,25 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
 public class ques_2{
+
+	int wordInSen(String word , String sentence){
+		int count =0;
+		StringTokenizer st =new StringTokenizer(sentence);;
+		while(st.hasMoreTokens()){
+			if(st.nextToken().equals(word)){
+				count++;
+			}
+		}
+		return count;
+	}
 	public static void main(String args[]){
-		
+		Scanner sc =new Scanner(System.in);
+		String word =sc.nextLine();
+		String sentence =sc.nextLine();
+		ques_2 wordInSen = new ques_2();
+		int n =wordInSen.wordInSen(word , sentence);
+		System.out.println(n);
 	}
 }
